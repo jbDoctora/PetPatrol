934f7031e69c72a2733abe9f2d461a2e3605b80b
jbDoctora <doctorajillbert@gmail.com>
2023-03-14 18:42:05 +0800

    m

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,6 +1,6 @@
 @if (session()->has('message'))
 <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 2000)" x-show="show"
-    class="bg-green-300 fixed top-0 left-1/2 -translate-x-1/2 transform rounded-lg px-48 py-3 text-black"
+    class="bg-green-300 fixed top-0 left-1/2 -translate-x-1/2 transform rounded-sm px-48 py-3 text-black"
     x-transition:enter="transition-transform transition-opacity ease-out duration-300"
     x-transition:enter-start="opacity-0 transform translate-x-2"
     x-transition:enter-end="opacity-100 transform translate-y-0" x-transition:leave="transition ease-in duration-300"
