09546cc0b3e0ad97ed9edaf228bde4dffc5a84fa
jbDoctora <doctorajillbert@gmail.com>
2023-04-20 09:03:15 +0800

    small ui change

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,2 +1,30 @@
 <x-admin-layout>
+    <div class="grid grid-cols-12 gap-4 p-4">
+
+        <!-- Cards -->
+        <div class="col-span-6 sm:col-span-4 lg:col-span-2 bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-lg font-bold mb-2">Total Users</h2>
+            <div clas>3</div>
+        </div>
+        <div class="col-span-6 sm:col-span-4 lg:col-span-2 bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-lg font-bold mb-2">Card 2</h2>
+            <p>Content goes here</p>
+        </div>
+        <div class="col-span-6 sm:col-span-4 lg:col-span-2 bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-lg font-bold mb-2">Card 3</h2>
+            <p>Content goes here</p>
+        </div>
+        <div class="col-span-6 sm:col-span-4 lg:col-span-2 bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-lg font-bold mb-2">Card 4</h2>
+            <p>Content goes here</p>
+        </div>
+        <div class="col-span-6 sm:col-span-4 lg:col-span-2 bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-lg font-bold mb-2">Card 5</h2>
+            <p>Content goes here</p>
+        </div>
+        <div class="col-span-6 sm:col-span-4 lg:col-span-2 bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-lg font-bold mb-2">Card 6</h2>
+            <p>Content goes here</p>
+        </div>
+    </div>
 </x-admin-layout>
