68d058e56c3ed9374ac94b20ec3c53dfd9d8ff3b
jbDoctora <doctorajillbert@gmail.com>
2023-04-23 10:39:20 +0800

    Added notification queuing

--- app/Notifications/GcashPaymentNotification.php
+++ app/Notifications/GcashPaymentNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use App\Models\Booking;
 
-class GcashPaymentNotification extends Notification
+class GcashPaymentNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
